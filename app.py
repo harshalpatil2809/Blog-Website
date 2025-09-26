c64053e42407d2cfe33650720488e2bed1085fb7
harshalpatil2809 <harshalpatil28095@gmail.com>
2025-09-26 08:54:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SECRET_KEY"] = "BLOG-by-harshal"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(200), unique = True, nullable = False)
+    password = db.Column(db.String(200),  nullable = False)
+
+class Blogs(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    text = db.Column(db.Text, nullable = False)
+    author = db.Column(db.String(100), nullable = False)
+    created_at = db.Column(db.DateTime, default = datetime.utcnow)
+    
+
+with app.app_context():
+    db.create_all()
+
+
+
+@app.route("/")
+def home():
+    return render_template("base.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
