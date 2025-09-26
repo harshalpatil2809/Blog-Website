ca41b8d535592788f1ea5f426ff8add270ea0a37
harshalpatil2809 <harshalpatil28095@gmail.com>
2025-09-26 17:27:40 +0530

    creating db

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
-from flask import Flask,render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,render_template,url_for,redirect
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,EmailField,SubmitField
+from wtforms.validators import DataRequired,Email,ValidationError
+import bcrypt
+from flask_mysqldb import MYSQL
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
-app.config["SECRET_KEY"] = "BLOG-by-harshal"
-db = SQLAlchemy(app)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "harshalpatil@123"
+app.config["MYSQL_DB"] = "blogdb"
+app.secret_key = "blog-by-harshal"
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(200), unique = True, nullable = False)
-    password = db.Column(db.String(200),  nullable = False)
+mysql = MYSQL(app)
 
-class Blogs(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(100), nullable = False)
-    text = db.Column(db.Text, nullable = False)
-    author = db.Column(db.String(100), nullable = False)
-    created_at = db.Column(db.DateTime, default = datetime.utcnow)
-    
-
-with app.app_context():
-    db.create_all()
-
+class Registration(FlaskForm):
+    name = StringField("Name", DataRequired())
+    email = EmailField("Email", DataRequired(), Email())
+    password = PasswordField("password", DataRequired())
+    submit = SubmitField("submit")
 
 
 @app.route("/")
@@ -33,8 +30,24 @@ def home():
 def post():
     return render_template("post.html")
 
-@app.route("/login")
-def login():
+@app.route("/registration")
+def registration():
+    form  = Registration()
+    if form.validate_on_submit:
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+
+        hashed_pass = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+
+        cursor = MYSQL.connection.cursor()
+
+        cursor.execute(" INSERT INTO (name,email,password) VALUES (%s,%s,%s)",(name,email,password))
+        mysql.connect.commit()
+        cursor.close()
+
+        return redirect(url_for("login"))
+
     return render_template("register.html")
 
 
